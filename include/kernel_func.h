c2cb6545e38d9a1b2796f0a68a23ba321aeec746
Ruiqi <ruiqi134@gmail.com>
2021-12-17 21:54:00 -0500

    add Spiky kernel

--- include/kernel_func.h
+++ include/kernel_func.h
@@ -8,4 +8,9 @@ double poly6(double r);
 
 void dPoly6(Eigen::Vector3d &dC, Eigen::Ref<const Eigen::Vector3d> d, double r);
 
+
+double spiky(double r);
+
+void dSpiky(Eigen::Vector3d &dW, Eigen::Ref<const Eigen::Vector3d> d, double r);
+
 #endif
--- src/kernel_func.cpp
+++ src/kernel_func.cpp
@@ -10,7 +10,25 @@ double poly6(double r){
     return std::pow(RADIUS * RADIUS - r * r, 3) * POLY6_COEF;
 }
 
-void dPoly6(Eigen::Vector3d &dC, Eigen::Ref<const Eigen::Vector3d> d, double r){
-    dC = GRAD_POLY_COEF * std::pow(RADIUS  * RADIUS - r * r, 2) * d;
+void dPoly6(Eigen::Vector3d &dW, Eigen::Ref<const Eigen::Vector3d> d, double r){
+    dW = GRAD_POLY_COEF * std::pow(RADIUS  * RADIUS - r * r, 2) * d;
+}
+
+
+double SPIKY_COEF = 15.0 / (M_PI * std::pow(RADIUS, 6));
+
+double GRAD_SPIKY_COEF = - 45.0 / (M_PI * std::pow(RADIUS, 6));
+
+
+double spiky(double r){
+    return SPIKY_COEF * std::pow(RADIUS - r, 3);
+}
+
+void dSpiky(Eigen::Vector3d &dW, Eigen::Ref<const Eigen::Vector3d> d, double r){
+    if(r < 1e-7){
+        dW = Eigen::Vector3d::Zero();
+    }else{
+        dW = GRAD_SPIKY_COEF * std::pow(RADIUS - r, 2) / r * d;
+    }
 }
 
